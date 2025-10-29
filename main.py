f55bfac2c5b474ddd5da6e0bc950c46e8e60294e
Vasu-Devs <siddhvasudev1402@gmail.com>
2025-10-30 02:32:34 +0530

    removed fgbg and only using mediapipe

--- main.py
+++ main.py
@@ -4,61 +4,44 @@ import numpy as np
 import string
 import os
 
-
-
-mp_selfie_segentation = mp.solutions.selfie_segmentation
-segmentation = mp_selfie_segentation.SelfieSegmentation(model_selection=1)
-
-fgbg = cv2.createBackgroundSubtractorMOG2(history=200, varThreshold=20 , detectShadows=False)
+mp_selfie_segmentation = mp.solutions.selfie_segmentation
+segmentation = mp_selfie_segmentation.SelfieSegmentation(model_selection=1)
 
 chars = np.array(list(string.printable[:94]))
-font_ratio=0.45
+font_ratio = 0.45
 
 cap = cv2.VideoCapture(0)
 
 while cap.isOpened():
-    ret , frame = cap.read()
+    ret, frame = cap.read()
     if not ret:
         break
 
-    frame= cv2.resize(frame,(120,48))
-    h,w,_ = frame.shape
-
+    frame = cv2.resize(frame, (120, 48))
+    h, w, _ = frame.shape
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # frame= cv2.flip(frame ,1)
-    
-    rgb = cv2.cvtColor(frame , cv2.COLOR_BGR2RGB)
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     result = segmentation.process(rgb)
+
     mask_mp = result.segmentation_mask
-    masp_mp = (mask_mp>0.5).astype(np.uint8)
+    mask_mp = (mask_mp > 0.5).astype(np.uint8) * 255  # binary mask of person only
 
-    mask_fg = fgbg.apply(frame)
-    mask_fg = cv2.threshold(mask_fg, 200,1,cv2.THRESH_BINARY)[1]
-
-    mask_fg = cv2.resize(mask_fg, (frame.shape[1], frame.shape[0]))
-    mask_mp = cv2.resize(mask_mp, (frame.shape[1], frame.shape[0]))
-    mask_mp = (mask_mp * 255).astype(np.uint8)
-    mask_fg = (mask_fg * 255).astype(np.uint8)
-
-    combined = cv2.bitwise_or(mask_mp, mask_fg)
-
-
-    combined=cv2.medianBlur(combined,5)
-
-    _ , combined = cv2.threshold(combined, 200,255, cv2.THRESH_BINARY)
+    # smooth mask for cleaner borders
+    mask_mp = cv2.medianBlur(mask_mp, 5)
 
     ascii_frame = ""
-    for y in range(0,h):
-        for x in range(0,w):
-            if combined[y,x]>0:
-                brightness = gray[y,x]
-                char = chars[int(brightness/256*len(chars))]
-                ascii_frame+=char
+    for y in range(h):
+        for x in range(w):
+            if mask_mp[y, x] > 0:
+                brightness = gray[y, x]
+                char = chars[int(brightness / 256 * len(chars))]
+                ascii_frame += char
             else:
-                ascii_frame+="."
-        ascii_frame+="\n"
-    os.system('cls' if os.name=='nt' else 'clear')
+                ascii_frame += "."
+        ascii_frame += "\n"
+
+    os.system('cls' if os.name == 'nt' else 'clear')
     print(ascii_frame)
 
 cap.release()
